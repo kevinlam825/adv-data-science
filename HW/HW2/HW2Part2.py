f3164460dbe703a7c768c7425681e8d71318c13d
kevinlam825 <kevin.lam825@gmail.com>
2018-03-19 16:48:04 -0700

    added base file

---
+++ HW/HW2/HW2Part2.py
@@ -0,0 +1,20 @@
+
+# coding: utf-8
+
+# In[1]:
+
+#Importing necessary libraries
+import matplotlib.image as mpimg
+import matplotlib.pyplot as plt
+
+
+# In[2]:
+
+#To allow images to be shown inside Jupyter
+get_ipython().magic('matplotlib inline')
+
+
+# In[ ]:
+
+
+
